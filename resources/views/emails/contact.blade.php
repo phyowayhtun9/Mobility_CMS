c22bdbaa28a2f519633e95d7e4014cab8124db00
Phyo Wai Tun <103883855+phyowayhtun9@users.noreply.github.com>
2023-05-24 19:01:41 +0630

    Add files via upload

---
+++ resources/views/emails/contact.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <meta charset="utf-8">
+</head>
+<body>
+
+<div>
+    Name: <?php echo $name?><br/>
+    Email: <?php echo $email ?><br/>    
+    Message: <?php echo $user_message ?><br/>
+</div>
+
+</body>
+</html>
